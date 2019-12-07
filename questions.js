4fc51e647aaa38ea8a28dbc4d42b7c7e5e3cdf86
Nancy Alter <nancy.alter@gmail.com>
2019-12-07 00:43:29 -0500

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,56 @@
+var questions = [
+    {
+        id: 1,
+        title: "What is an array?",
+        choices: ["A list of string variables.", "Related integers.", "A sequence of values.", "Groups of things."],
+        answer: "A sequence of values."
+    },
+    {
+        id: 1,
+        title: "What is the purpose of a div tag in HTML?",
+        choices: ["Highlight text.", "Make areas bold.", "Draw borders.", "Create different sections."],
+        answer: "Create different sections."
+    },
+    {
+        id: 1,
+        title: "Which HTML attribute is used to define inline styles?",
+        choices: ["style", "class", "styles", "font"],
+        answer: "style"
+    },
+    {
+        id: 1,
+        title: "The Bootstrap grid system is based on how many columns?",
+        choices: ["6 columns", "12 columns", "9 columns", "3 columns"],
+        answer: "12 columns"
+    },
+    {
+        id: 2,
+        title: "According to the AKC The Border Collie is",
+        choices: ["considered a Working Dog.", "a member of the Herding Group.", "a member of the Hunting Group.", "a family dog."],
+        answer: "a member of the Herding Group."
+    },
+    {
+        id: 2,
+        title: "The Border Collie Controls it's herd ",
+        choices: ["by nipping at the feet.", "through soft barking.", "by threatening growls.", "with an intense stare."],
+        answer: "with an intense stare."
+    },
+    {
+        id: 2,
+        title: "The average lifespan of a Border Collie is",
+        choices: ["12 - 15 years", "10 - 12 years", "8 - 10 years", "6 - 12 years"],
+        answer: "12 - 15 years"
+    },
+    {
+        id: 2,
+        title: "The Border Collie is generally",
+        choices: ["affectionate with family and strangers.", "reserved with family and strangers.", "affectionate with family and reserved with strangers.", "aggressive toward strangers."],
+        answer: "affectionate with family and reserved with strangers."
+    }
+    
+];
+
+ 
+
+  
+ 
